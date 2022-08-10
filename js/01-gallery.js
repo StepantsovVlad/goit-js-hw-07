d2ad453414f04f10657a47eb40557e62ae233c95
StepantsovVlad <92571283+StepantsovVlad@users.noreply.github.com>
2022-08-10 20:35:56 +0300

    gallery collect

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", galleryItem());
+function galleryItem() {
+    const markup = galleryItems.map(
+        ({ original, preview, description }) =>
+            `<div class="gallery__item">
+         <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+          </a>
+       </div>`)
+        .join('');
+    return markup;
+
+};
+
+let instance = null;
+
+gallery.addEventListener("click", event => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    instance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}" alt="${event.target.alt}">`);
+    instance.show();
+    document.addEventListener('keydown', modalClose);
+});
+
+function modalClose(event) {
+    if (event.code === "Escape" && instance.visible()) {
+        instance.close();
+    }
+    document.removeEventListener('keydown', modalClose);
+};
