a8f9bb397ff9292170dcf6d35387743dbedbd5d5
StepantsovVlad <92571283+StepantsovVlad@users.noreply.github.com>
2022-08-11 08:10:43 +0300

    completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+function createEl(original, preview, description) {
+    return `<a class="gallery__item" href="${preview}">
+    <img class="gallery__image" src="${original}" alt="${description}" />
+  </a>`
+
+};
+
+const galleryStringEl = galleryItems.reduce((contanier, galleryItem) => {
+    const { preview, original, description } = galleryItem;
+
+    contanier = contanier + createEl(preview, original, description);
+    return contanier;
+}, "");
+
+galleryEl.insertAdjacentHTML("beforeend", galleryStringEl);
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
